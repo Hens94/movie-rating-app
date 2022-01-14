f0233ce5f64f07acaf17f882bd9b0b2eb3dc5cd2
Henry Gadea <henry_gadea@hotmail.com>
2022-01-14 16:16:23 -0600

    Create constants to handle application messages

---
+++ src/constants/index.js
@@ -0,0 +1,58 @@
+// Theme
+export const THEME_SET = "THEME_SET";
+export const THEMES = {
+  DEFAULT: "DEFAULT",
+  DARK: "DARK",
+  LIGHT: "LIGHT",
+  BLUE: "BLUE",
+  GREEN: "GREEN",
+  INDIGO: "INDIGO",
+};
+
+// General
+export const SUCCESS = "success";
+export const ERROR = "error";
+export const WARNING = "warning";
+
+// Auth
+export const AUTH_SIGN_IN_REQUEST = "AUTH_SIGN_IN_REQUEST";
+export const AUTH_SIGN_IN_SUCCESS = "AUTH_SIGN_IN_SUCCESS";
+export const AUTH_SIGN_IN_FAILURE = "AUTH_SIGN_IN_FAILURE";
+export const AUTH_SIGN_OUT = "AUTH_SIGN_OUT";
+export const AUTH_GET_CURRENT_AUTHENTICATED_USER =
+  "AUTH_GET_CURRENT_AUTHENTICATED_USER";
+
+// Messages
+export const GET_MOVIES_SORRY =
+  "sorry, something went wrong getting the movies data";
+export const CREATED_MOVIES_SORRY =
+  "sorry, something went wrong creating the movie data";
+export const UPDATED_MOVIES_SORRY =
+  "sorry, something went wrong saving the movie data";
+export const DELETED_MOVIES_SORRY =
+  "sorry, something went wrong deleting the movie data";
+export const CREATED_MOVIES_SUCCESS = "Movie created successfully";
+export const UPDATED_MOVIES_SUCCESS = "Movie saved successfully";
+export const DELETED_MOVIES_SUCCESS = "Movie deleted successfully";
+
+export const GET_WATCHLIST_SORRY =
+  "sorry, something went wrong getting the watchlist data";
+export const ADD_WATCHLIST_SORRY =
+  "sorry, something went wrong adding the watchlist data";
+export const DELETE_WATCHLIST_SORRY =
+  "sorry, something went wrong removing the watchlist data";
+export const ADD_WATCHLIST_SUCCESS = "Movie added to watchlist successfully";
+export const DELETE_WATCHLIST_SUCCESS =
+  "Movie removed to watchlist successfully";
+
+export const GET_RATINGS_SORRY =
+  "sorry, something went wrong getting the rating data";
+export const GET_GENRE_SORRY =
+  "sorry, something went wrong getting the genre data";
+export const GET_RATINGS_ADD =
+  "Thanks for your rating, it will be added to the movie ratings";
+export const GET_COMMENT_ADD =
+  "Thanks for your comment, it will be added to the movie comments";
+export const DELETED_RATING_SORRY =
+  "sorry, something went wrong deleting the rating data";
+export const DELETE_RATING_SUCCESS = "Rating deleted successfully";
