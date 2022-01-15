d49beb001bba4af40026138e67fa924a3214bad0
Henry Gadea <henry_gadea@hotmail.com>
2022-01-14 19:15:46 -0600

    Fix watchlist page title

--- src/pages/WatchList/index.js
+++ src/pages/WatchList/index.js
@@ -1,4 +1,4 @@
-import { Divider as MuiDivider } from "@material-ui/core";
+import { Divider as MuiDivider, Typography } from "@material-ui/core";
 import { spacing } from "@material-ui/system";
 import styled from "styled-components/macro";
 import { Helmet } from "react-helmet-async";
@@ -11,6 +11,9 @@ const WatchList = () => {
   return (
     <>
       <Helmet title="Watchlist" />
+      <Typography variant="h3" gutterBottom display="inline">
+        Watchlist
+      </Typography>
       <Divider my={6} />
       <MoviesListComponent />
     </>
